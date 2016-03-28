0e937805c4ead9c32b14a89b07e18f6546f29d9c
Chris West <cwest-engineer@outlook.com>
2016-03-27 23:50:13 -0400

    Import into github

---
+++ window.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#compare1').mergely({
+    width: 'auto',
+    height: 'auto',
+    cmsettings: {
+      readOnly: false, 
+      lineWrapping: true,
+    }
+  });
+});
